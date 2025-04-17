ed69ed9deae82fa62a704625a91e5597a3bd8afd
kike <egarciavicent@gmail.com>
2025-04-17 20:14:15 +0200

    feat : añade endpoint /productos para enviar datos al frontend

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-// index.js
-
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const mysql = require('mysql2');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hola mundo");
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.DB_PASSWORD,
+  database: 'zero11'
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Error al conectar con la base de datos:', err);
+    return;
+  }
+  console.log('Conexión a la base de datos MySQL establecida correctamente.');
+});
+
+app.get('/productos', (req, res) => {
+  connection.query('SELECT * FROM productos', (err, results) => {
+    if (err) {
+      res.status(500).json({ error: 'Error al obtener productos' });
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 app.listen(PORT, () => {
