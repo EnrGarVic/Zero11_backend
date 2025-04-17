ebecfeee97885197bacbf051634bea80d24ce71d
kike <egarciavicent@gmail.com>
2025-04-17 19:19:05 +0200

    feat: instalado nodejs, express y mysql y primer hola mundo para probar el servidor

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hola mundo");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
