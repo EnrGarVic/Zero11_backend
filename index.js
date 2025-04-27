78805786c0bd9c3145799afd768728d5dd52c87d
kike <egarciavicent@gmail.com>
2025-04-27 23:01:06 +0200

    feat: instalado jsonwebtoken para manejar el inicio de sesion del administrador con tokens

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-
-require('dotenv').config();
+require("dotenv").config();
 
 //importacion de dependencias
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const mysql = require('mysql2');
-const cors = require('cors');
-const bcrypt = require('bcrypt');
-
+const mysql = require("mysql2");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 app.use(cors());
 
@@ -16,26 +15,26 @@ app.use(express.json());
 
 // Conexión a la base de datos MySQL
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: "localhost",
+  user: "root",
   password: process.env.DB_PASSWORD,
-  database: 'zero11'
+  database: "zero11",
 });
 
 // Verificamos que la conexión se haya realizado correctamente
-connection.connect(err => {
+connection.connect((err) => {
   if (err) {
-    console.error('Error al conectar con la base de datos:', err);
+    console.error("Error al conectar con la base de datos:", err);
     return;
   }
-  console.log('Conexión a la base de datos MySQL establecida correctamente.');
+  console.log("Conexión a la base de datos MySQL establecida correctamente.");
 });
 
 //ebdpoint para obtener todos los productos
-app.get('/productos', (req, res) => {
-  connection.query('SELECT * FROM productos', (err, results) => {
+app.get("/productos", (req, res) => {
+  connection.query("SELECT * FROM productos", (err, results) => {
     if (err) {
-      res.status(500).json({ error: 'Error al obtener productos' });
+      res.status(500).json({ error: "Error al obtener productos" });
     } else {
       res.json(results);
     }
@@ -43,7 +42,7 @@ app.get('/productos', (req, res) => {
 });
 
 // Endpoint para obtener productos por categoría
-app.get('/categorias-con-productos', (req, res) => {
+app.get("/categorias-con-productos", (req, res) => {
   const sql = `
     SELECT
       categorias.id AS categoria_id,
@@ -58,17 +57,17 @@ app.get('/categorias-con-productos', (req, res) => {
 
   connection.query(sql, (err, results) => {
     if (err) {
-      return res.status(500).json({ error: 'Error al obtener los datos' });
+      return res.status(500).json({ error: "Error al obtener los datos" });
     }
 
     const categoriasMap = new Map();
 
-    results.forEach(row => {
+    results.forEach((row) => {
       if (!categoriasMap.has(row.categoria_id)) {
         categoriasMap.set(row.categoria_id, {
           id: row.categoria_id,
           nombre: row.categoria_nombre,
-          productos: []
+          productos: [],
         });
       }
 
@@ -76,7 +75,7 @@ app.get('/categorias-con-productos', (req, res) => {
         categoriasMap.get(row.categoria_id).productos.push({
           id: row.producto_id,
           nombre: row.producto_nombre,
-          precio: row.precio
+          precio: row.precio,
         });
       }
     });
@@ -86,67 +85,91 @@ app.get('/categorias-con-productos', (req, res) => {
 });
 
 //endpoint login de admin
-app.post('/login-admin', (req, res) => {
+app.post("/login-admin", (req, res) => {
   const { usuario, password } = req.body;
 
-  const sql = 'SELECT * FROM admin WHERE usuario = ?';
+  const sql = "SELECT * FROM admin WHERE usuario = ?";
   connection.query(sql, [usuario], (err, results) => {
-    if (err) return res.status(500).json({ error: 'Error en el servidor' });
-    if (results.length === 0) return res.status(401).json({ error: 'Usuario no encontrado' });
+    if (err) return res.status(500).json({ error: "Error en el servidor" });
+    if (results.length === 0)
+      return res.status(401).json({ error: "Usuario no encontrado" });
 
     const admin = results[0];
     //libreria para encriptar la contraseña
     bcrypt.compare(password, admin.password, (err, isMatch) => {
-      if (err) return res.status(500).json({ error: 'Error al verificar la contraseña' });
+      if (err)
+        return res
+          .status(500)
+          .json({ error: "Error al verificar la contraseña" });
 
       if (isMatch) {
-        res.json({ success: true, message: 'Login correcto' });
+        const token = jwt.sign(
+          { id: admin.id, usuario: admin.usuario }, 
+          process.env.JWT_SECRET, 
+          { expiresIn: "1h" } 
+        );
+        res.json({ success: true, token: token });
       } else {
-        res.status(401).json({ success: false, message: 'Contraseña incorrecta' });
+        res
+          .status(401)
+          .json({ success: false, message: "Contraseña incorrecta" });
       }
     });
   });
 });
 
 // Endpoint para añadir un nuevo producto a la bd
-app.post('/productos', (req, res) => {
+app.post("/productos", (req, res) => {
   const { nombre, precio, categoria_id } = req.body;
 
-  const sql = 'INSERT INTO productos (nombre, precio, categoria_id) VALUES (?, ?, ?)';
+  const sql =
+    "INSERT INTO productos (nombre, precio, categoria_id) VALUES (?, ?, ?)";
   const values = [nombre, precio, categoria_id];
 
   connection.query(sql, values, (err, result) => {
     if (err) {
-      console.error('Error al insertar producto:', err);
-      res.status(500).json({ success: false, message: 'Error al insertar producto' });
+      console.error("Error al insertar producto:", err);
+      res
+        .status(500)
+        .json({ success: false, message: "Error al insertar producto" });
     } else {
-      res.status(201).json({ success: true, message: 'Producto añadido correctamente', productoId: result.insertId });
+      res
+        .status(201)
+        .json({
+          success: true,
+          message: "Producto añadido correctamente",
+          productoId: result.insertId,
+        });
     }
   });
 });
 
 //endpoint obtener todas las categorias(para desplegable)
-app.get('/categorias', (req, res) => {
-  connection.query('SELECT * FROM categorias', (err, results) => {
+app.get("/categorias", (req, res) => {
+  connection.query("SELECT * FROM categorias", (err, results) => {
     if (err) {
-      return res.status(500).json({ error: 'Error al obtener categorías' });
+      return res.status(500).json({ error: "Error al obtener categorías" });
     }
     res.json(results);
   });
 });
 
-// endpoint para eliminar producto por ID
-app.delete('/productos/:id', (req, res) => {
+// Eliminar producto por ID
+app.delete("/productos/:id", (req, res) => {
   const id = req.params.id;
 
-  const sql = 'DELETE FROM productos WHERE id = ?';
+  const sql = "DELETE FROM productos WHERE id = ?";
 
   connection.query(sql, [id], (err, result) => {
     if (err) {
-      console.error('Error al eliminar producto:', err);
-      res.status(500).json({ success: false, message: 'Error al eliminar producto' });
+      console.error("Error al eliminar producto:", err);
+      res
+        .status(500)
+        .json({ success: false, message: "Error al eliminar producto" });
     } else {
-      res.status(200).json({ success: true, message: 'Producto eliminado correctamente' });
+      res
+        .status(200)
+        .json({ success: true, message: "Producto eliminado correctamente" });
     }
   });
 });
